390620d99ff4f0f802775f6b3d405db0ab40c005
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2022-09-29 17:29:12 -0500

    first work for watsco

--- src/index.ts
+++ src/index.ts
@@ -1,94 +1,135 @@
 import {
-  BooleanField,
-  DateField,
-  Message,
-  NumberField,
-  OptionField,
-  Portal,
-  Sheet,
-  TextField,
-  Workbook,
-} from '@flatfile/configure'
-
-import { FlatfileRecord, FlatfileRecords } from '@flatfile/hooks'
-import fetch from 'node-fetch'
-
-const Employees = new Sheet(
-  'Employees',
-  {
-    firstName: TextField({
-      required: true,
-      description: 'Given name',
-    }),
-    lastName: TextField({
-      compute: (v: any) => {
-        return `Rock`
-      },
-    }),
-    fullName: TextField(),
-
-    stillEmployed: BooleanField(),
-    department: OptionField({
-      label: 'Department',
-      options: {
-        engineering: { label: 'Engineering' },
-        hr: 'People Ops',
-        sales: 'Revenue',
-      },
-    }),
-    fromHttp: TextField({ label: 'Set by batchRecordCompute' }),
-    salary: NumberField({
-      label: 'Salary',
-      description: 'Annual Salary in USD',
-      required: true,
-      validate: (salary: number) => {
-        const minSalary = 30_000
-        if (salary < minSalary) {
+  //  BooleanField,
+    DateField,
+    Message,
+    NumberField,
+    OptionField,
+  //  Portal,
+    Sheet,
+    TextField,
+    Workbook,
+  } from '@flatfile/configure'
+  
+  // import { FlatfileRecord, FlatfileRecords } from '@flatfile/hooks'
+  // import fetch from 'node-fetch'
+  import crono from 'chrono-node'
+  
+  const Branch_Supplemental = new Sheet(
+    'Branch Supplemental',
+    {
+      branch_code: NumberField({
+        label: "Branch Code",
+        required: true,
+        unique: true,
+        description: 'Code of the branch',
+      }),
+      branch_name: TextField({
+        label: "Branch Name",
+        required: true
+      }),
+      branch_type: OptionField({
+        label: 'Branch Type',
+        options: {
+          cost_center: { label: 'Cost Center' },
+          other: { label: 'Other' },
+          field_operations: { label: 'Field Operations' },
+          sales_center: { label: 'Sales Center' },
+          hub: { label: 'Hub' },
+          distribution_center: { label: 'Distribution Center' }
+        },
+      }),
+      alt_region_code: TextField({
+        label: "Alt Region Code",
+        validate: (alt_region_code: string) => {
+        const alphaRegex = /^[a-z0-9]+$/i
+        if (!alphaRegex.test(alt_region_code)) {
           return [
             new Message(
-              `${salary} is less than minimum wage ${minSalary}`,
-              'warn',
+              `${alt_region_code} should be alphanumeric`,
+              'error',
               'validate'
             ),
           ]
         }
       },
-    }),
-    startDate: DateField()
-  },
-  {
-    allowCustomFields: true,
-    readOnly: true,
-    recordCompute: (record) => {
-      const fullName = `{record.get('firstName')} {record.get('lastName')}`
-      record.set('fullhName', fullName)
-      return record
-    },
-    batchRecordsCompute: async (payload: FlatfileRecords<any>) => {
-      const response = await fetch('https://api.us.flatfile.io/health', {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
+      }),
+      alt_region_name: TextField({
+        label: "Alt Region Name",
+        validate: (alt_region_name: string) => {
+          const alphaRegex = /^[a-z0-9]+$/i
+          if (!alphaRegex.test(alt_region_name)) {
+            return [
+              new Message(
+                `${alt_region_name} should be alphanumeric`,
+                'error',
+                'validate'
+              ),
+            ]
+          }
         },
-      })
-      const result = await response.json()
-      payload.records.map(async (record: FlatfileRecord) => {
-        record.set('fromHttp', result.info.postgres.status)
-      })
+      }),
+      branch_group_code: NumberField({
+        label: "Branch Group Code"
+      }),
+      branch_group_name: TextField({
+        label: "Branch Group Name"
+      }),
+      open_date: DateField({
+        label: "Open Date",
+        required: true
+      }),
+      close_date: DateField({
+        label: "Close Date"
+      }),
+      active_date: DateField({
+        label: "Active Date"
+      }),
+      last_relo_date: DateField({
+        label: "Last Relo Date"
+      }),
+      branch_status: OptionField({
+        label: 'Branch Status',
+        options: {
+          baker: { label: 'BAKER' },
+          tradewinds: { label: 'TRADEWINDS' },
+          consolidated: { label: 'Consolidated' }
+        },
+      }),
+      status_effective_date: DateField({
+        label: "Status Effective Date"
+      }),
+      status_end_date: DateField({
+        label: "Status End Date"
+      }),
+      notes: TextField({
+        label: "Notes"
+      }),
     },
-  }
-)
-
-const EmployeesPortal = new Portal({
-  name: 'EmployeesPortal',
-  sheet: 'Employees'
-})
-
-export default new Workbook({
-  name: 'Employees',
-  namespace: 'employee',
-  sheets: {
-    Employees,
-  },
-  portals: [EmployeesPortal],
-})
+  
+    
+    {
+      allowCustomFields: false,
+      readOnly: false,
+      // example of a record hook (not needed because of date typecasting)
+      // recordCompute: (record) => {
+      //   const openDate = crono.parse(`{record.get('open_date')}`)
+      //   record.set('open_date', openDate[0].start.date())
+      //   return record
+      // }
+    }
+  )
+  
+  // Portal not needed in this example
+  // const Supplemental3Portal = new Portal({
+  //   name: 'Supplemental3Portal',
+  //   sheet: 'Supplemental3'
+  // })
+  
+  export default new Workbook({
+    name: 'Branch Supplemental',
+    namespace: 'Branch Supplemental',
+    sheets: {
+      Branch_Supplemental,
+    },
+    // portals: [Supplemental3Portal],
+  })
