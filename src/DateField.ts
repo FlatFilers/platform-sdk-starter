b2547b250e279d9acb6e1b54fa4c9a1dfd05547d
Paddy Mullen <paddy@paddymullen.com>
2022-10-04 18:50:22 -0400

    working on a non triply extended field

--- src/DateField.ts
+++ src/DateField.ts
@@ -1,5 +1,5 @@
 import * as chrono from 'chrono-node'
-import { Field, FullBaseFieldOptions } from '@flatfile/configure/stdlib/CastFunctions'
+import { Field, GenericDefaults, SchemaILField, FieldHookDefaults, FullBaseFieldOptions } from '@flatfile/configure/stdlib/CastFunctions'
 
 export const StringCast = (raw: string | undefined | null): string | null => {
   if (typeof raw === 'undefined') {
@@ -34,8 +34,41 @@ export const ChronoDateCast = StringCastCompose(ChronoStringDateCast)
 
 
 type O = Record<string, any>
-type PartialBaseFieldsAndOptions = Partial<FullBaseFieldOptions<Date, O>>
-export DateField = (options?: string | PartialBaseFieldsAndOptions) => {
+type T = Date
+type PartialBaseFieldsAndOptions = Partial<FullBaseFieldOptions<T, O>>
+export const DateField = (options?: string | PartialBaseFieldsAndOptions) => {
+    // if labelOptions is a string, then it is the label
+    const label = typeof options === 'string' ? options : undefined
+    // if options is an object, then it is the options
+    const passedOptions =
+      (typeof options !== 'string' ? options : options) ?? {}
 
-  const field = new Field<Date, O>(fullOpts as FullBaseFieldOptions<Date, O>)
+    const passedStageVisibility = (passedOptions as SchemaILField)
+      ?.stageVisibility
+
+    const stageVisibility = {
+      ...GenericDefaults.stageVisibility,
+      ...passedStageVisibility,
+    }
+
+    const passedAnnotations = (passedOptions as SchemaILField)?.annotations
+
+    const annotations = {
+      ...GenericDefaults.annotations,
+      ...passedAnnotations,
+    }
+
+    const fullOpts = {
+      ...GenericDefaults,
+      ...FieldHookDefaults<T>(),
+      cast:ChronoDateCast, 
+      ...(label ? { label } : { ...passedOptions }),
+      stageVisibility,
+      annotations,
+    }
+
+  const field = new Field<T, O>(fullOpts as FullBaseFieldOptions<T, O>)
+  return field
 }
+
+
