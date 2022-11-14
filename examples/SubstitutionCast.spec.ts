456ad5afecf5186de994c1ea2fa0d5c988a22021
Paddy Mullen <paddy@paddymullen.com>
2022-11-14 10:35:42 -0500

    feat: add descriptive comments

--- examples/SubstitutionCast.spec.ts
+++ examples/SubstitutionCast.spec.ts
@@ -1,21 +1,32 @@
 import { Sheet, TextField, Workbook, SubstitutionCast } from '@flatfile/configure'
 import { SheetTester } from '../src/utils/testing/SheetTester'
 
-const numberSet = [
+
+
+
+const spanishNumeralSynonyms = [
   ['1', 'one', 'un'],
   ['2', 'two', 'dos'],
 ]
 
-const SpanishNum = SubstitutionCast(
-  numberSet,
+
+const SpanishNumeralCast = SubstitutionCast(
+  spanishNumeralSynonyms,
   2,
   (val) => `Couldn't convert '${val}' to a spanish number`
 )
 
+// Substitution cast is used to coerce synonym values to a desired
+// value.  In this case '1', 'one', and 'un' are synonyms.  If any of
+// the three are found in a field using this cast, the value is set as
+// the last item of the list.  Substitution cast compares string  case
+// insensitive.
+
+
 // note sheet must have same name as key in workbook it is shared as
 const SubSheet = new Sheet(
   'SubSheet',
-  {numField: TextField({cast:SpanishNum})})
+  {numField: TextField({cast:SpanishNumeralCast})})
 
 const TestWorkbook = new Workbook({
   name: `Test Workbook`,
@@ -25,11 +36,15 @@ const TestWorkbook = new Workbook({
 })
 
 describe('Workbook tests ->', () => {
+  // here we use Sheet tester
   const testSheet = new SheetTester(TestWorkbook, 'SubSheet')
   test('Spanish number word works', async () => {
+    // for this inputRow
     const inputRow = { numField: 'un'}
+    // we expect this output row
     const expectedOutputRow = { numField: 'un'}
     const res = await testSheet.testRecord(inputRow)
+    //call the expectation here
     expect(res).toMatchObject(expectedOutputRow)
   })
 
