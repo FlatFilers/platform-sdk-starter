916ccd74aed3ec64dba3ce9c5c057aef338d5529
syshinnn <seungyeon.009@gmail.com>
2022-11-18 14:18:30 -0600

    adds first dateCompare test case

--- examples/dateCompare.spec.ts
+++ examples/dateCompare.spec.ts
@@ -38,21 +38,30 @@ const TestWorkbook = new Workbook({
   sheets: { RequiredWhenSheet},
 })
 
+/*
+We have a bunch of use cases where we want to compare two or more dates in a row and throw an error if a condition is not met. 
+For example, we want "Open Date" to always be before "Close Date" or both fields should throw the same error. 
+This would need to come after we do the previous validations (code you sent over) for date format on the field level. How would I do this on the platform SDK?
+
+Expected behavior:
+1. Attempt to fix Date 1 and Date 2 format if it is not in the correct YYYY-MM-DD format
+- If a fix cannot be made, throw an error relating to the date format needing to be fixed, but do not throw an error relating to the comparison of the two dates. (Code you sent over previously)
+2.  Only if  Date 1 and Date 2 are both in the correct format (either by autoformatting it or manually changing the date in the UI), run the comparison logic and throw a new error on both dates if Date 1 is before Date 2.
+*/
+
 describe('Workbook tests ->', () => {
 
   const rqTestSheet = new SheetTester(TestWorkbook, 'RequiredWhenSheet')
   test('RequiredWhen test1', async () => {
     // for this inputRow
-    const inputRow = { a:'wont trigger', b:null }
-    // we expect this output row
-    const expectedOutputRow = { a:'wont trigger', b:null }
-    const res = await rqTestSheet.testRecord(inputRow)
-    const res2 = await rqTestSheet.testMessage(inputRow)
+    // testing to see whether a is before b
+    // expected to error here
+    const inputRow = { a:'2022-07-30', b:'2022-06-30' }
+    const messages = await rqTestSheet.testMessage(inputRow)
 
     //use the match functions like
-    expect(matchSingleMessage(res2, 'b')).toBeFalsy()
-
-    expect(res).toMatchObject(expectedOutputRow)
+    expect(matchSingleMessage(messages, 'b', "date b cannot be before date a", 'error')).toBeTruthy()
+    expect(matchSingleMessage(messages, 'a', "date b cannot be before date a", 'error')).toBeTruthy()
   })
 
   test('RequiredWhen test2', async () => {
