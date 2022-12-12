8ca167ee3c2977f98a947675df7a68e22996db6c
Eric Crane <eric.crane10@gmail.com>
2022-12-12 15:36:27 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,131 @@
 /**
- * This is a scaffold for defining a Workbook with Sheets and Portals.
- * Test this scaffold using the sample file in examples/sample-uploads/my-sheet-sample.csv.
- *
- * See examples/workbooks/FullExample.ts for a full, working example of a Workbook.
+ * Example workbook for importing employees
+ * Test with the sample file at examples/sample-uploads/employees.csv
  */
-
-import {
+ import {
+  BooleanField,
+  DateField,
+  Message,
   NumberField,
+  OptionField,
   Portal,
   Sheet,
   TextField,
   Workbook,
+  LinkedField,
 } from '@flatfile/configure'
 
-/**
- * Sheets
- * Define your Sheet configuration and Fields here, or import them:
- * import { YourSheet } from './path-to-your-sheet/your-sheet.ts'
- */
-const MySheet = new Sheet('MySheet', {
-  firstName: TextField(),
-  lastName: TextField(),
-  age: NumberField(),
-})
+import { FlatfileRecord, FlatfileRecords } from '@flatfile/hooks'
+import fetch from 'node-fetch'
 
-/**
- * Portals
- * Define your Portals here, or import them:
- * import { YourPortal } from './path-to-your-portal/your-portal.ts'
- */
-// const MyPortal = new Portal({
-//   name: 'MyPortal',
-//   sheet: 'MySheet',
+const BaseSheet = new Sheet(
+  'BaseSheet',
+  {
+    firstName: TextField({
+      primary: true,
+    }),
+    middleName: TextField('Middle'),
+    lastName: TextField(),
+    email: TextField({
+      unique: true,
+    }),
+  },
+  {
+    previewFieldKey: 'email',
+  }
+)
+
+// const LinkedSheet = new Sheet('LinkedSheet', {
+//   email: LinkedField({
+//     unique: true,
+//     label: 'Email',
+//     primary: true,
+//     sheet: BaseSheet,
+//   }),
+//   firstName: TextField(),
+//   middleName: TextField('Middle'),
+//   lastName: TextField(),
 // })
 
-// Workbook  - Update to reference your Workbook with Sheet(s) and Portal(s)
-export default new Workbook({
-  name: 'MyWorkbook',
-  namespace: 'my-workbook',
-  // portals: [MyPortal],
-  sheets: {
-    MySheet,
+const Employees = new Sheet(
+  'Employees',
+  {
+    firstName: TextField({
+      label: 'First Name',
+      required: true,
+      description: 'Given name',
+    }),
+    lastName: TextField({
+      compute: (v: any) => {
+        return `Rock`
+      },
+    }),
+    fullName: TextField(),
+
+    stillEmployed: BooleanField(),
+    department: OptionField({
+      label: 'Department',
+      options: {
+        engineering: { label: 'Engineering' },
+        hr: 'People Ops',
+        sales: 'Revenue',
+      },
+    }),
+    fromHttp: TextField({ label: 'Set by batchRecordCompute' }),
+    salary: NumberField({
+      label: 'Salary',
+      description: 'Annual Salary in USD',
+      required: true,
+      validate: (salary: number) => {
+        const minSalary = 30_000
+        if (salary < minSalary) {
+          return [
+            new Message(
+              `${salary} is less than minimum wage ${minSalary}`,
+              'warn',
+              'validate'
+            ),
+          ]
+        }
+      },
+    }),
+    startDate: DateField(),
   },
+  {
+    allowCustomFields: true,
+    readOnly: true,
+    recordCompute: (record) => {
+      const fullName = `${record.get('firstName')} ${record.get('lastName')}`
+      record.set('fullName', fullName)
+      return record
+    },
+    batchRecordsCompute: async (payload: FlatfileRecords<any>) => {
+      const response = await fetch('https://api.us.flatfile.io/health', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+        },
+      })
+      const result = (await response.json()) as any
+      payload.records.map(async (record: FlatfileRecord) => {
+        record.set('fromHttp', result.info.postgres.status)
+      })
+    },
+  }
+)
+
+// const EmployeesPortal = new Portal({
+//   name: 'EmployeesPortal',
+//   sheet: 'Employees',
+// })
+
+export default new Workbook({
+  name: 'Employees',
+  namespace: 'employee',
+  sheets: {
+    Employees,
+    BaseSheet,
+    // LinkedSheet,
+  },
+  // portals: [EmployeesPortal],
 })
