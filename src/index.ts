2e5bfe1a6a35f2d47ef35b0eb9fa486b921380fd
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2022-12-22 10:37:46 -0600

    Ridgeline Updates
    Ridgeline Updates

--- src/index.ts
+++ src/index.ts
@@ -11,32 +11,157 @@ import {
   TextField,
   Workbook,
 } from '@flatfile/configure'
+import { createReferenceId } from '../src/common/common'
 
-const BasicSheet = new Sheet(
-  'CF BasicSheet',
+
+const Models = new Sheet(
+  'Models',
   {
-    id: NumberField({
+      referenceId: TextField({
+          label: 'Reference Id',
+          description: 'Reference Id of Model',
+          required: true,
+          unique: false,
+          annotations: {
+              default: false,
+          },
+          //stageVisibility: {
+             // mapping: false,
+              //review: true,
+              //export: true
+          //}
+      }),
+
+      name: TextField({
+          label: 'Name',
+          description: 'Name of Model',
+          required: true,
+          unique: true,
+          annotations: {
+              default: false,
+          },
+          // stageVisibility: {
+          //     mapping: true,
+          //     review: true,
+          //     export: true
+          // }
+      }),
+
+      targetStyle: OptionField({
+          label: 'Target Style',
+          description: 'must be "PERCENTAGE"',
+          required: true,
+          unique: false,
+          options: {
+              PERCENTAGE: 'PERCENTAGE',
+          },
+          annotations: {
+              default: false,
+          },
+          //stageVisibility: {
+            //  mapping: true,
+              //review: true,
+              //export: true
+          //}
+      }),
+
+      valuation: OptionField({
+          label: 'Valuation',
+          required: true,
+          description: 'Valuation of Model',
+          unique: false,
+          options: {
+              STATIC: 'STATIC',
+              DYNAMIC: 'DYNAMIC',
+          },
+          annotations: {
+              default: false,
+          },
+          //stageVisibility: {
+            //  mapping: true,
+              //review: true,
+              //export: true
+          //}
+      }),
+
+      toleranceType: OptionField({
+          label: 'Tolerance Type',
+          required: true,
+          unique: false,
+          options: {
+              FLOATING: 'FLOATING',
+          },
+          annotations: {
+              default: false,
+          },
+          // stageVisibility: {
+          //     mapping: true,
+          //     review: true,
+          //     export: true
+          // }
+      }),
+  },
+  {
+      recordCompute: (record, _session, _logger) => {
+          createReferenceId("INDEX", 'name', 'referenceId', record)
+      },
+  },
+)
+
+const AssetClass = new Sheet(
+  'Asset Class',
+  {
+    assetClassReferenceId: TextField({
+      label: 'Asset Class Reference Id',
+      required: false,
+      unique: true,
+      description: 'Reference Id of the Asset Class',
+      annotations: {
+        default: false,
+              },
+      // stageVisibility: {
+      //   mapping: false,
+      //   review: true,
+      //   export: true
+      // }
+    }),
+    name: TextField({
+      label: 'Asset Class Name',
       required: true,
       unique: true,
+      description: 'Name of the Asset Class',
+      annotations: {
+        default: false,
+              },
+      // stageVisibility: {
+      //   mapping: true,
+      //   review: true,
+      //   export: true
+      // }
     }),
-    firstName: TextField({
+    legacyCodeAssetClass: TextField({
+      label: 'Asset Class Legacy Code',
       required: true,
-      description: 'Given name',
-    }),
-    lastName: TextField(),
-    fullName: TextField(),
-    startDate: DateField(),
-    isActive: BooleanField(),
-    department: OptionField({
-      label: 'Department',
-      options: {
-        engineering: 'Engineering',
-        hr: 'People Ops',
-        sales: 'Revenue',
-      },
-    }),
-  }
+      unique: true,
+      description: 'Legacy Short code for the asset class',
+      annotations: {
+        default: false,
+              },
+      // stageVisibility: {
+      //   mapping: true,
+      //   review: true,
+      //   export: false
+      // }
+    })
+  },
+  {
+    previewFieldKey: "assetClassReferenceId",
+    recordCompute: (record, _session, _logger) => {
+      createReferenceId("ASSET_CLASS", 'name', 'assetClassReferenceId', record)
+    },
+  },
 )
+
 
 // const BasicSheetPortal = new Portal({
 //   name: 'BasicSheetPortal',
@@ -44,13 +169,14 @@ const BasicSheet = new Sheet(
 // })
 
 export default new SpaceConfig({
-  name: 'CF Basic',
+  name: 'Ridgeline Testing',
   workbookConfigs: {
     'basic':new Workbook({
-      name: 'BasicSheetWorkbook',
-      namespace: 'basic',
+      name: 'RidgelineTest',
+      namespace: 'Ridgeline',
       sheets: {
-        BasicSheet,
+        Models,
+        AssetClass,
       },
       // portals: [BasicSheetPortal],
     })
