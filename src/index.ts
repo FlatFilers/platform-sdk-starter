4408de90c5fa87eff2dfb8b67ceeda6225e17db0
Paddy Mullen <paddy@paddymullen.com>
2023-01-18 17:37:41 -0500

    reduced diff to main

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,3 @@
-import {
-  TextField,
-  NumberField,
-  GroupByField,
-  Sheet,
-  Workbook,
-  Portal
-} from '@flatfile/configure'
-import {
-  Group,
-  SumField,
-  Count,
-  Match,
-  Error,
-  GreaterThan,
-  Unless,
-  GroupConstraintItem,
-  Do
-} from './expression-lang/EXPR'
-
 /**
  * This is a scaffold for defining a Workbook with Sheets and Portals.
  * Test this scaffold using the sample file in examples/sample-uploads/my-sheet-sample.csv.
@@ -25,6 +5,13 @@ import {
  * See examples/workbooks/FullExample.ts for a full, working example of a Workbook.
  */
 
+import {
+  NumberField,
+  Portal,
+  Sheet,
+  TextField,
+  Workbook,
+} from '@flatfile/configure'
 
 /**
  * Sheets
@@ -37,37 +24,6 @@ const MySheet = new Sheet('MySheet', {
   age: NumberField(),
 })
 
-const BothSheet =  new Sheet(
-  'BothSheet',
-  {
-    name: TextField(), 
-    eye_color: TextField(), 
-    job: TextField(), 
-    age: NumberField(),
-    age_sum: GroupByField(
-      ['job'],
-      // NOTE THE USE OF DO HERE...
-      // Do allows multiple expressions to be executed and the result
-      // of the last one is returned... in this case "SumField"
-      Do(
-       GroupConstraintItem(
-	  Group(),
-	  Unless(
-	    GreaterThan(
-	      Count(Match({eye_color: 'blue_'}, Group())),
-	      0),
-	    Error('No Blue eyes')),
-	  'name',
-	  Group()),
-	SumField(Group(), 'age'))
-    ),
-  }
-)
-
-
-
-
-
 /**
  * Portals
  * Define your Portals here, or import them:
@@ -75,13 +31,15 @@ const BothSheet =  new Sheet(
  */
 const MyPortal = new Portal({
   name: 'MyPortal',
-  sheet: 'BothSheet',
+  sheet: 'MySheet',
 })
 
 // Workbook  - Update to reference your Workbook with Sheet(s) and Portal(s)
-//export default new Workbook({
-export default new Workbook({name: 't', namespace: 't', sheets: {BothSheet},
-  // name: 'MyWorkbook',
-  // namespace: 'my-workbook',
+export default new Workbook({
+  name: 'MyWorkbook',
+  namespace: 'my-workbook',
   portals: [MyPortal],
+  sheets: {
+    MySheet,
+  },
 })
